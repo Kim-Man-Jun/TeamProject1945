4fa34852afcb90c653e77331c278890c8a68a860
cientodos <wjsrlqor777@gmail.com>
2023-07-24 16:18:43 +0900

    1차백업

--- Assets/102/Script/SelectPlayer.cs
+++ Assets/102/Script/SelectPlayer.cs
@@ -6,14 +6,14 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class SelectPlayer : MonoBehaviour
 {
-    public GameObject main;
-    public GameObject s1;
-    public GameObject s2;
+    public GameObject main; //메인화면 이미지
+    public GameObject s1; //1번 캐릭 이미지
+    public GameObject s2; // 이하 동문
     public GameObject s3;
     public GameObject s4;
 
-    public int SelectNum = 0;
-    public bool isMain = true;
+    public int SelectNum = 0; //캐릭 선택 번호 0 > 1번 따라서 3 > 4번
+    public bool isMain = true; //메인이미지 활성화 되어있는지
     void Start()
     {
         
@@ -23,23 +23,23 @@ public class SelectPlayer : MonoBehaviour
     void Update()
     {
    
-        if(isMain == true) { 
-        if (Input.anyKeyDown)
+        if(isMain == true) { //한번만 실행되게하기위함
+        if (Input.anyKeyDown) //아무키나 누르면
         {  
 
              main.SetActive(false);
              isMain = false;   
         }
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow)) 
+        if (Input.GetKeyDown(KeyCode.DownArrow)) //아래키 누르면 내려감
         {
             SelectNum += 1;
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow)) //위키 누르면 올라감
         {
             SelectNum -= 1;
         }
-        if(SelectNum == 0) 
+        if(SelectNum == 0)  //0이면 1번 캐릭실행
         {
             if (isMain == false) 
             { 
