205581a68fda6700c4445946596a2f399ceb8743
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-25 13:01:25 +0900

    07.25 1차

---
+++ Assets/KMJ/Enemy/EnemyController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject Target_Player;
+
+    public float Speed = 5f;
+    Vector2 dir;
+    Vector2 dirNo;
+    public GameObject BoomEffect;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        Target_Player = GameObject.FindGameObjectWithTag("Player");
+        dir = Target_Player.transform.position - transform.position;
+        dirNo = dir.normalized;
+
+        rb.AddForce(dirNo * Speed * Time.deltaTime, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
