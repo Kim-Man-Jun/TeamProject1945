002a3fe3731e9a8bb61380b9e11faf18d23c14bf
hyunsu98 <108320515+hyunsu98@users.noreply.github.com>
2023-07-26 15:30:28 +0900

    7/26 백업

--- Assets/LHS/Scripts/LHS_Monster1.cs
+++ Assets/LHS/Scripts/LHS_Monster1.cs
@@ -3,36 +3,41 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+//1단계 적
+//플레이어를 따라다닌다 -> 일정한 간격을 둔다 (애니메이션)
+//총을 발사한다.
 public class LHS_Monster1 : MonoBehaviour
 {
-    //플레이어를 따라다니며 죽일예정 ~~~ 
+    [Header("이동속도")]
+    [SerializeField] float speed;
 
-    // 다시 돌아올 타겟
-    public Transform target;
-    public float speed;
-
+    GameObject target;
     Animator anim;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
-    {
-        float h = transform.position.x;
-        float v = transform.position.y;
+    { 
+        #region 애니메이션
+        //벡터의 뺄셈 이용한 애니메이션 처리
+        //플레이어가 어느 방향쪽에 있는지 체크 
+        //이후 애니메이션 바꾸기
+        Vector3 dir = target.transform.position - transform.position;
 
-        //작아지면 Right 
-        if( h >= 0.05f )
+        if(dir.x > 0.5f )
         {
+            //+ 오른쪽으로 가야함
             anim.SetBool("Right", true);
         }
         else
         {
             anim.SetBool("Right", false);
         }
-        if( h <= -0.05f)
+        if (dir.x < 0.5f)
         {
             anim.SetBool("Left", true);
         }
@@ -40,10 +45,10 @@ public class LHS_Monster1 : MonoBehaviour
         {
             anim.SetBool("Left", false);
         }
+        #endregion
 
-        //Vector3 dir = target.position - transform.position;
-        transform.position = Vector3.Lerp(transform.position, target.transform.position - new Vector3(2, 0, 0), speed);
+        //transform.position = Vector3.Lerp(transform.position, target.transform.position - new Vector3(2, 0, 0), speed);
 
-        //transform.Translate(dir * speed * Time.deltaTime);
+        transform.Translate(dir * speed * Time.deltaTime);
     }
 }
