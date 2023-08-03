d5b0d99aa88f62d33dea25a90359f76c785fd034
cientodos <wjsrlqor777@gmail.com>
2023-08-03 16:22:30 +0900

    kjl

--- Assets/TotalGm.cs
+++ Assets/TotalGm.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TotalGm : MonoBehaviour
 {
     public static TotalGm instance = null;
 
-    public bool isClear1 = false;
-    public bool isClear2 = false;
-    public bool isClear3 = false;
+    public bool isClear1 = true;
+    public bool isClear2 = true;
+    public bool isClear3 = true;
     public bool isClear4 = false;
     public bool isMain = true; //메인이미지 활성화 되어있는지
     public string playingPlayer;
@@ -26,6 +27,14 @@ public class TotalGm : MonoBehaviour
 
         // 이렇게 하면 다음 scene으로 넘어가도 오브젝트가 사라지지 않습니다.
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (isClear1 == true && isClear2 == true && isClear3 == true && isClear4 == true)
+        {
+            SceneManager.LoadScene("Ending");
+        }
     }
 
 
